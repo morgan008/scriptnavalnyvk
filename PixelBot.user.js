4c20b858c57add3cb9151c7a2c3ffcd9435c0f9a
topchik1 <33516909+topchik1@users.noreply.github.com>
2017-11-09 14:27:05 +0200

    Update PixelBot.user.js

--- PixelBot.user.js
+++ PixelBot.user.js
@@ -15,7 +15,7 @@ function CoiPixelBot() {
 
     CoiPixelBot.url = {
         script: 'https://github.com/topchik1/scriptnavalnyvk/raw/master/PixelBot.user.js',
-        image:  'https://i.imgur.com/dezqebB.png'
+        image:  'https://i.imgur.com/gGnNrse.png'
     };
 
     CoiPixelBot.refreshTime = 60;
